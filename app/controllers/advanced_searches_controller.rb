728d4f088628786336a11b48863cd947e3dfa2d2
Adoniran França <adoniranfranca@gmail.com>
2023-11-12 14:56:13 -0300

    Melhorando pesquisa avançada

--- app/controllers/advanced_searches_controller.rb
+++ app/controllers/advanced_searches_controller.rb
@@ -4,20 +4,33 @@ class AdvancedSearchesController < ApplicationController
   end
 
   def search
-    inns = Inn.active.includes(:rooms).order(:brand_name)
-    filter_params = {
-      accepts_pets: "accepts_pets",
-      private_bathroom: "rooms.private_bathroom",
-      balcony: "rooms.balcony",
-      air_conditioning: "rooms.air_conditioning",
-      tv: "rooms.tv",
-      wardrobe: "rooms.wardrobe",
-      safe_available: "rooms.safe_available",
-      accessible_for_disabled: "rooms.accessible_for_disabled"
-    }
-
-    filter_params.each do |attribute, param_name|
-      @inns = inns.where(param_name => true) if params[attribute] == "true"
+    @inns = Inn.active.order(:brand_name)
+    if params[:advanced_query]
+      @inns = @inns.search_for_inns(params[:advanced_query])
+    end
+    if params[:accepts_pets]
+      @inns = @inns.where(accepts_pets: true)
+    end
+    if params[:private_bathroom]
+      @inns = @inns.joins(:rooms).where('rooms.private_bathroom = true')
+    end
+    if params[:balcony]
+      @inns = @inns.joins(:rooms).where('rooms.balcony = true')
+    end
+    if params[:air_conditioning]
+      @inns = @inns.joins(:rooms).where('rooms.air_conditioning = true')
+    end
+    if params[:tv]
+      @inns = @inns.joins(:rooms).where('rooms.tv = true')
+    end
+    if params[:wardrobe]
+      @inns = @inns.joins(:rooms).where('rooms.wardrobe = true')
+    end
+    if params[:safe_available]
+      @inns = @inns.joins(:rooms).where('rooms.safe_available = true')
+    end
+    if params[:accessible_for_disabled]
+      @inns = @inns.joins(:rooms).where('rooms.accessible_for_disabled = true')
     end
   end
 end
--- spec/system/inns/visit_advanced_search_for_inns_spec.rb
+++ spec/system/inns/visit_advanced_search_for_inns_spec.rb
@@ -27,7 +27,10 @@ describe 'Visitante faz busca avançada de pousadas' do
     within('nav') do
       click_on 'Busca Avançada'
     end
-    click_on 'Aceita Pets'
+    within('section#advanced_search_form') do
+      check 'Aceita Pets'
+      click_on 'Buscar'
+    end
     
     # Assert
     expect(page).to have_content('Pousada Boa Vista')
@@ -61,14 +64,17 @@ describe 'Visitante faz busca avançada de pousadas' do
     within('nav') do
       click_on 'Busca Avançada'
     end
-    click_on 'TV'
+    within('section#advanced_search_form') do
+      check 'TV'
+      click_on('Buscar')
+    end
     
     # Assert
     expect(page).not_to have_content('Pousada Boa Vista')
     expect(page).not_to have_content('Pousada Árvore da Coruja')
   end
 
-  it 'e busca pousadas que tenham quartos com ar condicionado' do
+  it 'e busca pousadas que tenham quartos com ar condicionado com texto' do
     # Arrange
     admin = Admin.create!(name: 'Admin', email: 'admin@admin.com', password: 'password')
     guesthouse = Inn.create!(admin: admin, brand_name: 'Pousada Boa Vista', corporate_name: 'Pousada Good LTDA',
@@ -78,7 +84,7 @@ describe 'Visitante faz busca avançada de pousadas' do
                             description: 'Pousada de frente para o rio.',
                             payment_methods: 'Crédito e Débito', accepts_pets: true,
                             usage_policies: 'Não é permitido fumar', check_in: '15:00', check_out: '14:00', status: :active)
-    Inn.create!(admin: admin, brand_name: 'Pousada Árvore da Coruja', corporate_name: 'Pousada Guest LTDA',
+    other_guesthouse = Inn.create!(admin: admin, brand_name: 'Pousada Árvore da Coruja', corporate_name: 'Pousada Guest LTDA',
                             registration_number: '24469244000186', phone: '(99)94444-1234', email: 'arvere@email.com.br',
                             address: 'Rua: Pedro Candiago, 725', neighborhood: 'Planalto', state: 'RS',
                             city: 'Gramado', zip_code: ' 95670-000',
@@ -88,16 +94,48 @@ describe 'Visitante faz busca avançada de pousadas' do
     Room.create!(inn: guesthouse, title: 'Chalé de 1 Quarto', description: 'Camas confortáveis', dimension: 40 , max_occupancy: 4,
                 daily_rate: 300, private_bathroom: true, balcony: true, air_conditioning: true,
                 tv: false, wardrobe: true, safe_available: true, accessible_for_disabled: true, for_reservations: :unavailable)
+    Room.create!(inn: other_guesthouse, title: 'Bangalô Família', description: 'Com vista para o rio e barcos de pesca', dimension: 35,
+                max_occupancy: 6, daily_rate: 300, private_bathroom: true, balcony: false, air_conditioning: true,
+                tv: true, wardrobe: true, safe_available: true, accessible_for_disabled: true, for_reservations: :available)
 
     # Act
     visit root_path
     within('nav') do
       click_on 'Busca Avançada'
     end
-    click_on 'Ar Condicionado'
-    
+    within('section#advanced_search_form') do
+      fill_in 'Digite o nome fantasia', with: 'Boa'
+      check 'Aceita Pets'
+      check 'Ar Condicionado'
+      click_on 'Buscar'
+    end
     # Assert
     expect(page).to have_content('Pousada Boa Vista')
     expect(page).not_to have_content('Pousada Árvore da Coruja')
+  end
+
+  it 'e não existe pousada de acordo com a especificação' do
+    # Arrange
+    admin = Admin.create!(name: 'Admin', email: 'admin@admin.com', password: 'password')
+    guesthouse = Inn.create!(admin: admin, brand_name: 'Pousada Boa Vista', corporate_name: 'Pousada Good LTDA',
+                            registration_number: '94769049000157', phone: '(99)12122-1234', email: 'boavista@email.com.br',
+                            address: 'Rua: Marcelo Déda', neighborhood: 'Centro', state: 'SE',
+                            city: 'Canindé de São Francisco', zip_code: ' 95670-000',
+                            description: 'Pousada de frente para o rio.',
+                            payment_methods: 'Crédito e Débito', accepts_pets: true,
+                            usage_policies: 'Não é permitido fumar', check_in: '15:00', check_out: '14:00', status: :active)
+    # Act
+    visit root_path
+    within('nav') do
+      click_on 'Busca Avançada'
+    end
+    within('section#advanced_search_form') do
+      check 'Aceita Pets'
+      check 'Ar Condicionado'
+      click_on 'Buscar'
+    end
+    # Assert
+    expect(page).to have_content('Nenhuma pousada encontrada')
+    expect(page).not_to have_content('Pousada Boa Vista')
   end
 end
