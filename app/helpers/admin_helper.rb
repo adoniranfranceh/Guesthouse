d101eaf204866505d4e3c97af6bac456d2635cb2
Adoniran França <adoniranfranca@gmail.com>
2023-11-21 23:29:21 -0300

    alterando nav do layout

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,9 +1,9 @@
 module AdminHelper
   def dynamic_link_inn
     if admin_signed_in? && current_admin.inn.try(:id).present?
-      link_to 'Minha Pousada', admin_show_inn_path(current_admin.inn.id)
+      link_to 'Minha Pousada', admin_show_inn_path(current_admin.inn.id), class: "nav-link"
     else
-      link_to 'Cadastrar Pousada', new_inn_path
+      link_to 'Cadastrar Pousada', new_inn_path, class: "nav-link"
     end
   end
 end
